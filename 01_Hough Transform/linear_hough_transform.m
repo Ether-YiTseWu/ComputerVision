39d6c0b4ba7ac14a6cb07dbd0b41e3bc1e562559
tailer954 <44743933+tailer954@users.noreply.github.com>
2020-03-18 20:30:10 +0800

    Linear Hough (matlab Ver)

---
+++ 01_Hough Transform/linear_hough_transform.m
@@ -0,0 +1,26 @@
+imgRGB = imread("road.jpg");
+imgGray = rgb2gray(imgRGB);
+
+imgBlur = imgaussfilt(imgGray, 10);
+imgEdge = edge(imgBlur, 'canny');
+[H, T, R] = hough(imgEdge, 'RhoResolution', 0.5, 'ThetaResolution', 0.5);
+peakValue = houghpeaks(H, 10);
+lines = houghlines(imgEdge, T, R, peakValue,'FillGap',1000,'MinLength',3)
+
+figure(1);
+subplot(1,4,1); imshow(imgRGB);  title("RGB Image");
+subplot(1,4,2); imshow(imgGray); title("Gray Image");
+subplot(1,4,3); imshow(imgBlur); title("Blur Image");
+subplot(1,4,4); imshow(imgEdge); title("Edge Image");
+
+figure(2);
+imshow(imadjust(mat2gray(H))); title("Image After Hough Transform"); 
+axis on, axis normal, hold on;
+
+figure(3);
+imshow(imgRGB); hold on; title("Detect Result");
+for k = 1:length(lines)
+    % 繪製各條線
+    xy = [lines(k).point1; lines(k).point2];
+    plot(xy(:,1),xy(:,2),'LineWidth',3,'Color','green');   
+end

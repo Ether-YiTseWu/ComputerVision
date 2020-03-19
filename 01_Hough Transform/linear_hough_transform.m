9fa8896e664311fa0de3a734aaeea7b3fd9c64f0
tailer954 <44743933+tailer954@users.noreply.github.com>
2020-03-19 20:46:14 +0800

    Linear Hough Transform

--- 01_Hough Transform/linear_hough_transform.m
+++ 01_Hough Transform/linear_hough_transform.m
@@ -1,6 +1,7 @@
 imgRGB = imread("road.jpg");
 imgGray = rgb2gray(imgRGB);
 
+imgEdge_bad = edge(imgGray, 'canny');
 imgBlur = imgaussfilt(imgGray, 10);
 imgEdge = edge(imgBlur, 'canny');
 [H, T, R] = hough(imgEdge, 'RhoResolution', 0.5, 'ThetaResolution', 0.5);
@@ -8,10 +9,11 @@ peakValue = houghpeaks(H, 10);
 lines = houghlines(imgEdge, T, R, peakValue,'FillGap',1000,'MinLength',3)
 
 figure(1);
-subplot(1,4,1); imshow(imgRGB);  title("RGB Image");
-subplot(1,4,2); imshow(imgGray); title("Gray Image");
-subplot(1,4,3); imshow(imgBlur); title("Blur Image");
-subplot(1,4,4); imshow(imgEdge); title("Edge Image");
+subplot(1,5,1); imshow(imgRGB);  title("RGB Image");
+subplot(1,5,2); imshow(imgGray); title("Gray Image");
+subplot(1,5,3); imshow(imgBlur); title("Blur Image");
+subplot(1,5,4); imshow(imgEdge); title("Edge Image");
+subplot(1,5,5); imshow(imgEdge_bad); title("Bad Edge Image");
 
 figure(2);
 imshow(imadjust(mat2gray(H))); title("Image After Hough Transform"); 

753c9bfc03f68892265a04a961e14bae2318597a
tailer954 <44743933+tailer954@users.noreply.github.com>
2020-04-30 17:57:51 +0800

    SVM src

---
+++ 05_SVM/svm_for_iris.m
@@ -0,0 +1,34 @@
+clear; clc;
+
+load fisheriris
+
+idx = ~strcmp(species, 'setosa');
+feature = meas(:, 3:4);
+classifier = fitcsvm(feature, idx);
+supportVector = classifier.SupportVectors;
+
+figure(1); title("Iris Data")
+for i = min(feature(:,1)):0.05:max(feature(:,1))+1
+    for j = min(feature(:,2)):0.05:max(feature(:,2))+1
+        [result, score] = predict(classifier, [i, j]);
+        if result == 0
+            plot(i, j, 'g*'); hold on;
+        elseif result == 1
+            plot(i, j, 'c*'); hold on;
+        end
+        if score <= 0.05
+            plot(i, j, 'k*'); hold on;
+        end
+    end
+end
+
+for i = 1:size(feature, 1)
+    if idx(i) == 0
+        plot(feature(i,1), feature(i,2), 'ro'); hold on;
+    else
+        plot(feature(i,1), feature(i,2), 'bo'); hold on;
+    end
+end
+
+sv = plot(supportVector(:,1),supportVector(:,2),'k+','MarkerSize',10);
+legend(sv, "Support Vector");
